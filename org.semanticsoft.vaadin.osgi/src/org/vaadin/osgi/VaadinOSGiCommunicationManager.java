58f2876d3544f306c77b87db744bdf4c8f5262aa
Rushan Gilmullin <rus-reg@yandex.ru>
2012-11-02 02:40:32 +0400

    This project contains the VaadinOSGiServlet from Apple Inc. I take it as a base for own Servlet. In new servlet implemented own comminucation manager that have feature bellow. We can add runnables in any vaadin listener by calling method invokeLater. The runnable will be executed after the vaadin's variable change end. This is similarity SwingUtilites.invokeLater and swt Display.asyncExec.

---
+++ org.semanticsoft.vaadin.osgi/src/org/vaadin/osgi/VaadinOSGiCommunicationManager.java
@@ -0,0 +1,53 @@
+package org.vaadin.osgi;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import com.vaadin.Application;
+import com.vaadin.terminal.VariableOwner;
+import com.vaadin.terminal.gwt.server.CommunicationManager;
+
+public class VaadinOSGiCommunicationManager extends CommunicationManager
+{
+	//List<Runnable> runnables = new ArrayList<>();
+	Queue<Runnable> runnables = new LinkedList<>();
+	
+	public VaadinOSGiCommunicationManager(Application application)
+	{
+		super(application);
+		VaadinOSGiApplicationManager.getInstance().setCommunicationManager(this);
+	}
+	
+	protected void changeVariables(Object source, final VariableOwner owner, Map<String, Object> m) {
+        super.changeVariables(source, owner, m);
+        
+        exec();
+    }
+	
+	private synchronized void exec()
+	{
+		Runnable runnable;
+    	while ((runnable = runnables.poll()) != null)
+        {
+        	try
+			{
+        		runnable.run();
+			}
+			catch (Throwable e)
+			{
+				e.printStackTrace();
+			}
+        }
+	}
+
+	public synchronized void invokeLater(Runnable runnable)
+	{
+		this.runnables.add(runnable);
+	}
+	
+	
+}
