84a19af1708e8f1ad145d37bf64f01f0de994e81
Rushan Gilmullin <rus-reg@yandex.ru>
2012-11-06 15:55:31 +0400

    Statusbar added (as a trim contribution with toolcontrol in bottom trimbar)

---
+++ org.semanticsoft.vaaclipsedemo.cassandra.app/src/org/semanticsoft/vaaclipsedemo/cassandra/app/toolitems/StatusBar.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Rushan R. Gilmullin and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Rushan R. Gilmullin - initial API and implementation
+ *******************************************************************************/
+
+package org.semanticsoft.vaaclipsedemo.cassandra.app.toolitems;
+
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.Label;
+import javax.inject.Inject;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+
+public class StatusBar
+{
+	@Inject
+	public void EditModeToolItem(ComponentContainer parent, IEclipseContext context)
+	{
+		//the parent - is toolcontrol widget, we must add style for layouting in trimbar to parent widget, not to user control
+		//parent.addStyleName("editmode");
+		
+		final Label label = new Label("Ready");
+		parent.addComponent(label);
+	}
+}
