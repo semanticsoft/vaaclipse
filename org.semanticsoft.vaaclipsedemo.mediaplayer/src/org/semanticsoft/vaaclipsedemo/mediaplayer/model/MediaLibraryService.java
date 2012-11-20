4f374f02a358235136f24b35b180beb99c932b82
Rushan R. Gilmullin <semanticsoft.org@gmail.com>
2012-11-20 11:46:21 +0400

    add service methods

--- org.semanticsoft.vaaclipsedemo.mediaplayer/src/org/semanticsoft/vaaclipsedemo/mediaplayer/model/MediaLibraryService.java
+++ org.semanticsoft.vaaclipsedemo.mediaplayer/src/org/semanticsoft/vaaclipsedemo/mediaplayer/model/MediaLibraryService.java
@@ -3,6 +3,8 @@
  */
 package org.semanticsoft.vaaclipsedemo.mediaplayer.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,5 +13,67 @@ import java.util.List;
  */
 public class MediaLibraryService
 {
+	public static String getId(MediaEntry entry)
+	{
+		if (entry instanceof MediaLibrary)
+			return "library";
+		List<MediaCategory> pathToParent = getPathToParent(entry);
+		Collections.reverse(pathToParent);
+		String id = "/";
+		for (MediaCategory c : pathToParent)
+		{
+			id += c.getName() + "/";
+		}
+		
+		if (entry instanceof Media)
+			id += ((Media) entry).getUri();
+		else
+			id = id.substring(0, id.length()-1);
+		
+		return id;
+	}
+
+	private static List<MediaCategory> getPathToParent(MediaEntry entry)
+	{
+		List<MediaCategory> path = new ArrayList<>();
+		
+		MediaEntry curEntry = entry;
+		while (!(curEntry instanceof MediaLibrary))
+		{
+			if (curEntry instanceof MediaCategory)
+				path.add((MediaCategory) curEntry);
+			curEntry = curEntry.getParent();
+		}
+		
+		return path;
+	}
+
+	public static List<Media> getAllInnerMedia(MediaCategory mediaCat)
+	{
+		List<Media> medialist = new ArrayList<>();
+		for (MediaCategory childCat : mediaCat.getCategories())
+		{
+			medialist.addAll(getAllInnerMedia(childCat));
+		}
+		medialist.addAll(mediaCat.getMediaList());
+		return medialist;
+	}
 	
+	public static Media findMediaById(MediaCategory mediaCat, String id, String path)
+	{
+		for (Media m : mediaCat.getMediaList())
+		{
+			if (id.equals(path + m.getUri()))
+				return m;
+		}
+		
+		for (MediaCategory childCat : mediaCat.getCategories())
+		{
+			Media m = findMediaById(childCat, id, path + childCat.getName() + "/");
+			if (m != null)
+				return m;
+		}
+		
+		return null;
+	}
 }
