fe5bbb672b4902cfc4a7b6cfb309ad47cc7e9df2
Sopot Cela <sopotcela@gmail.com>
2012-11-22 23:15:34 +0100

    Removed minor debugging traces.

--- org.semanticsoft.e4.injector.objectsuppliers/src/org/semanticsoft/e4/injector/objectsuppliers/CustomUIEventObjectSupplier.java
+++ org.semanticsoft.e4.injector.objectsuppliers/src/org/semanticsoft/e4/injector/objectsuppliers/CustomUIEventObjectSupplier.java
@@ -1,10 +1,8 @@
 package org.semanticsoft.e4.injector.objectsuppliers;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.suppliers.IObjectDescriptor;
 import org.eclipse.e4.ui.internal.di.UIEventObjectSupplier;
 
@@ -13,10 +11,6 @@ public class CustomUIEventObjectSupplier extends UIEventObjectSupplier {
 	@Inject
 	@Named("e4ApplicationInstanceId")
 	private String eAI;
-	
-	public void postConstruct(IEclipseContext context){
-		System.err.println("UIEVENT");
-	}
 	
 	@Override
 	protected String getTopic(IObjectDescriptor descriptor) {
