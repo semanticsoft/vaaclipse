a2b178fadad68a7bcdf7f5e45b1209662ac1a43d
Rushan R. Gilmullin <semanticsoft.org@gmail.com>
2012-11-24 17:39:57 +0400

    widget binaries udpate

--- org.semanticsoft.vaaclipse.vaadinaddons/org/semanticsoft/vaaclipse/widgets/SashWidgetVertical.java
+++ org.semanticsoft.vaaclipse.vaadinaddons/org/semanticsoft/vaaclipse/widgets/SashWidgetVertical.java
@@ -11,77 +11,45 @@
 
 package org.semanticsoft.vaaclipse.widgets;
 
-import java.util.Collection;
 import java.util.Map;
 
-import com.vaadin.terminal.gwt.client.ui.VSplitPanel;
-import com.vaadin.ui.Component;
 import com.vaadin.ui.VerticalSplitPanel;
 
 /**
  * @author rushan
  * 
  */
-public class SashWidgetVertical extends VerticalSplitPanel
+public class SashWidgetVertical extends VerticalSplitPanel implements SashWidget
 {
-	private SashLogic sashLogic = new SashLogic(this);
-
+	private SashWidgetExtension extension = new SashWidgetExtension(this);
+	
+	public void addListener(SplitPositionChangedListener listener)
+	{
+		this.extension.addListener(listener);
+	}
+	
 	public SashWidgetVertical()
 	{
 		this.setImmediate(true);
 	}
 	
 	@Override
-	public void setFirstComponent(Component c)
-	{
-		super.setFirstComponent(c);
-		if (sashLogic != null)
-			sashLogic.refreshState();
-	}
-	
-	@Override
-	public void setSecondComponent(Component c)
-	{
-		super.setSecondComponent(c);
-		if (sashLogic != null)
-			sashLogic.refreshState();
-	}
-	
-	public void refreshState()
-	{
-		if (sashLogic != null)
-			sashLogic.refreshState();
-	}
-	
-	@Override
-	public void childRequestedRepaint(Collection<RepaintRequestListener> alreadyNotified)
-	{
-		if (sashLogic != null)
-			sashLogic.refreshState();
-		super.childRequestedRepaint(alreadyNotified);
-	}
-	
-	@Override
 	public void changeVariables(Object source, Map<String, Object> variables)
 	{
-		try {
-			super.changeVariables(source, variables);	
-		}
-		catch (Exception e)
-		{
-			
-		}
+		super.changeVariables(source, variables);
+		
+		extension.fireEvent(this.getSplitPosition());
 
-		if (variables.containsKey("position") && !isLocked())
-		{
-			Float newPos = variables.get("position") instanceof Float ? (Float) variables.get("position") : ((Integer)variables.get("position")).floatValue();
-			setSplitPosition(newPos, UNITS_PERCENTAGE, false);
-		}
+//		if (variables.containsKey("position") && !isLocked())
+//		{
+//			Float newPos = variables.get("position") instanceof Float ? (Float) variables.get("position") : ((Integer)variables.get("position")).floatValue();
+//			setSplitPosition(newPos, UNITS_PERCENTAGE, false);
+//		}
 
-		if (variables.containsKey(VSplitPanel.SPLITTER_CLICK_EVENT_IDENTIFIER))
-		{
-			fireClick((Map<String, Object>) variables.get(VSplitPanel.SPLITTER_CLICK_EVENT_IDENTIFIER));
-		}
+//		if (variables.containsKey(VSplitPanel.SPLITTER_CLICK_EVENT_IDENTIFIER))
+//		{
+//			fireClick((Map<String, Object>) variables.get(VSplitPanel.SPLITTER_CLICK_EVENT_IDENTIFIER));
+//		}
 
 	}
 }
