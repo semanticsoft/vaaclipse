0a94eb37df5f88b511fd22820a467e7dba9a7ee0
Rushan R. Gilmullin <semanticsoft.org@gmail.com>
2012-12-28 02:17:25 +0400

    MediaCategory change

--- org.semanticsoft.vaaclipsedemo.mediaplayer/src/org/semanticsoft/vaaclipsedemo/mediaplayer/model/MediaCategory.java
+++ org.semanticsoft.vaaclipsedemo.mediaplayer/src/org/semanticsoft/vaaclipsedemo/mediaplayer/model/MediaCategory.java
@@ -24,22 +24,38 @@ public class MediaCategory extends MediaEntry
 		return Collections.unmodifiableList(mediaList);
 	}
 	
+	public List<MediaEntry> getMediaEntries()
+	{
+		List<MediaEntry> entry = new ArrayList<>();
+		entry.addAll(this.childCategories);
+		entry.addAll(this.mediaList);
+		return entry;
+	}
+	
 	public void addCategory(MediaCategory child)
 	{
 		child.setParent(this);
 		this.childCategories.add(child);
 	}
 	
-	public void removeMediaEntry(MediaEntry media)
-	{
-		this.mediaList.remove(media);
-		mediaEntryRemoved(media);
-	}
-	
 	public void addMedia(Media media)
 	{
 		media.setParent(this);
 		this.mediaList.add(media);
+	}
+	
+	public void addMediaEntry(MediaEntry mediaEntry)
+	{
+		if (mediaEntry instanceof MediaCategory)
+			this.addCategory((MediaCategory) mediaEntry);
+		else if (mediaEntry instanceof Media)
+			this.addMedia((Media) mediaEntry);
+	}	
+	
+	public void removeMediaEntry(MediaEntry media)
+	{
+		this.mediaList.remove(media);
+		mediaEntryRemoved(media);
 	}
 	
 	protected void mediaEntryRemoved(MediaEntry mediaEntry)
