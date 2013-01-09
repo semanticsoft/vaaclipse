b9b4906ad56262e9a8400fe647a3c596e0ed4d0e
Rushan R. Gilmullin <semanticsoft.org@gmail.com>
2013-01-09 07:01:18 +0400

    AreaRenderer: update top left folder using invokeLater; adding update on toberendered event...

--- org.semanticsoft.vaaclipse.presentation/src/org/semanticsoft/vaaclipse/presentation/renderers/AreaRenderer.java
+++ org.semanticsoft.vaaclipse.presentation/src/org/semanticsoft/vaaclipse/presentation/renderers/AreaRenderer.java
@@ -18,17 +18,21 @@ import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.advanced.MArea;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainerElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.workbench.IPresentationEngine;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.emf.ecore.EObject;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
+import org.semanticsoft.vaaclipse.api.VaadinExecutorService;
 import org.semanticsoft.vaaclipse.presentation.utils.HierarchyUtils;
 import org.semanticsoft.vaaclipse.widgets.StackWidget;
 import org.semanticsoft.vaaclipse.widgets.StackWidget.StateListener;
@@ -46,6 +50,9 @@ public class AreaRenderer extends VaadinRenderer
 {
 	@Inject
 	IEventBroker eventBroker;
+	
+	@Inject
+	VaadinExecutorService executorService;
 	
 	private EventHandler childrenHandler = new EventHandler() {
 		public void handleEvent(Event event)
@@ -70,51 +77,82 @@ public class AreaRenderer extends VaadinRenderer
 			else if (UIEvents.EventTypes.REMOVE.equals(eventType))
 				child = (MUIElement) event.getProperty(UIEvents.EventTags.OLD_VALUE);
 			
-			if (child == null || !child.isToBeRendered() || child.getWidget() == null)
+			if (child == null || !child.isToBeRendered())
 				return;
 			
-			MArea area = null;
-			//if it is area we directly process it
-			if (changedElement instanceof MArea)
-				area = (MArea) changedElement;
-			else
-			{//otherwise we find parent area
-				int loc = modelService.getElementLocation(changedElement);
-				if (loc != EModelService.IN_SHARED_AREA)
-					return;
-				
-				area = findArea(changedElement);	
-			}
+			if (!(child instanceof MPartStack || child instanceof MPartSashContainer))
+				return;
 			
-			if (area != null)
-			{
-				refreshTopRightState(area);
-			}
-		}
-
-		private MArea findArea(MUIElement element)
-		{
-			MUIElement parent = element.getParent();
-			while (parent != null)
-			{
-				if (parent instanceof MArea)
-					return (MArea) parent;
-				parent = parent.getParent();
-			}
-			return null;
+			processChangedElement(changedElement);
 		}
 	};
+	
+	private EventHandler toBeRenderedHandler = new EventHandler() {
+		public void handleEvent(Event event) {
+
+			MUIElement changedElement = (MUIElement) event
+					.getProperty(UIEvents.EventTags.ELEMENT);
+			
+			if (!(changedElement instanceof MPartStack || changedElement instanceof MPartSashContainer))
+				return;
+			
+			processChangedElement(changedElement);
+		}
+	};
+	
+	private void processChangedElement(MUIElement changedElement)
+	{
+		MArea area = null;
+		//if it is area we directly process it
+		if (changedElement instanceof MArea)
+			area = (MArea) changedElement;
+		else
+		{//otherwise we find parent area
+			int loc = modelService.getElementLocation(changedElement);
+			if (loc != EModelService.IN_SHARED_AREA)
+				return;
+			
+			area = findArea(changedElement);	
+		}
+		
+		if (area != null)
+		{
+			final MArea closureArea = area;
+			executorService.invokeLater(closureArea, new Runnable() {
+				
+				@Override
+				public void run()
+				{
+					refreshTopRightState(closureArea);
+				}
+			});
+		}
+	}
+
+	private MArea findArea(MUIElement element)
+	{
+		MUIElement parent = element.getParent();
+		while (parent != null)
+		{
+			if (parent instanceof MArea)
+				return (MArea) parent;
+			parent = parent.getParent();
+		}
+		return null;
+	}
 	
 	@PostConstruct
 	public void subscribe()
 	{
 		eventBroker.subscribe(UIEvents.ElementContainer.TOPIC_CHILDREN, childrenHandler);
+		eventBroker.subscribe(UIEvents.UIElement.TOPIC_TOBERENDERED, toBeRenderedHandler);
 	}
 	
 	@PreDestroy
 	public void unsubscribe()
 	{
 		eventBroker.unsubscribe(childrenHandler);
+		eventBroker.unsubscribe(toBeRenderedHandler);
 	}
 	
 	@Override
@@ -156,16 +194,6 @@ public class AreaRenderer extends VaadinRenderer
 		}
 		
 		refreshTopRightState(area);
-	}
-	
-	public static MArea findArea(MUIElement element) {
-		MUIElement parent = element.getParent();
-		while (parent != null) {
-			if (parent instanceof MArea)
-				return (MArea) parent;
-			parent = parent.getParent();
-		}
-		return null;
 	}
 	
 	/**
