94502ed109fc1cf8fceb7375a34d8710662b5d94
Rushan R. Gilmullin <semanticsoft.org@gmail.com>
2013-01-17 16:13:09 +0400

    Change application default widgetset to org.semanticsoft.vaaclipse.widgetsets.DefaultWidgetset

--- org.semanticsoft.vaaclipse.app/src/org/semanticsoft/vaaclipse/app/VaadinE4Application.java
+++ org.semanticsoft.vaaclipse.app/src/org/semanticsoft/vaaclipse/app/VaadinE4Application.java
@@ -160,9 +160,8 @@ public class VaadinE4Application implements IApplication, ResourceInfoProvider {
 
 		appWidgetset = appContext.getBrandingProperty("applicationWidgetset");
 		if (appWidgetset == null || appWidgetset.trim().isEmpty()) {
-			appWidgetset = "platform:/plugin/org.semanticsoft.vaaclipse.resources/VAADIN/widgetsets/vaaclipse_widgetset.widgetset.Vaaclipse_widgetsetWidgetset";
-			// appWidgetset =
-			// "platform:/plugin/com.vaadin.client-compiled/VAADIN/widgetsets/com.vaadin.DefaultWidgetSet";
+			appWidgetset = "platform:/plugin/org.semanticsoft.vaaclipse.widgetset.default/org.semanticsoft.vaaclipse.widgetsets.DefaultWidgetset";
+			//appWidgetset = "platform:/plugin/org.semanticsoft.vaaclipse.resources/VAADIN/widgetsets/vaaclipse_widgetset.widgetset.Vaaclipse_widgetsetWidgetset";
 		} else
 			appWidgetset = appWidgetset.trim();
 
