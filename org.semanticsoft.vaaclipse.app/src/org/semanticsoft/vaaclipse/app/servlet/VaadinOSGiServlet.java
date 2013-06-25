0e671359ec4a182d7f6639cab3f4f3e42240021d
Florian Pirchner <florian.pirchner@gmail.com>
2013-06-25 20:11:58 +0200

    Removed ICEPush support again! Does not work with jetty.

--- org.semanticsoft.vaaclipse.app/src/org/semanticsoft/vaaclipse/app/servlet/VaadinOSGiServlet.java
+++ org.semanticsoft.vaaclipse.app/src/org/semanticsoft/vaaclipse/app/servlet/VaadinOSGiServlet.java
@@ -16,17 +16,9 @@
  *******************************************************************************/
 package org.semanticsoft.vaaclipse.app.servlet;
 
-import java.io.IOException;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.icepush.servlet.MainServlet;
 import org.semanticsoft.vaaclipse.app.common.OSGiUIProvider;
-import org.vaadin.artur.icepush.ICEPush;
-import org.vaadin.artur.icepush.JavascriptProvider;
 
 import com.vaadin.server.DeploymentConfiguration;
 import com.vaadin.server.ServiceException;
@@ -42,9 +34,6 @@ import com.vaadin.server.VaadinSession;
 public class VaadinOSGiServlet extends VaadinServlet {
 
 	private static final long serialVersionUID = 1L;
-
-	private MainServlet mainservlet;
-	private JavascriptProvider javascriptProvider;
 
 	/**
 	 * Default constructor.
@@ -76,75 +65,5 @@ public class VaadinOSGiServlet extends VaadinServlet {
 				});
 		service.init();
 		return service;
-	}
-
-	@Override
-	public void init(ServletConfig servletConfig) throws ServletException {
-		try {
-			super.init(servletConfig);
-		} catch (ServletException e) {
-			if (e.getMessage().equals(
-					"Application not specified in servlet parameters")) {
-				// Ignore if application is not specified to allow the same
-				// servlet to be used for only push in portals
-			} else {
-				throw e;
-			}
-		}
-		mainservlet = new MainServlet(servletConfig.getServletContext());
-
-		try {
-			javascriptProvider = new JavascriptProvider(getServletContext()
-					.getContextPath());
-
-			ICEPush.setCodeJavascriptLocation(javascriptProvider
-					.getCodeLocation());
-		} catch (IOException e) {
-			throw new ServletException("Error initializing JavascriptProvider",
-					e);
-		}
-	}
-
-	@Override
-	protected void service(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String pathInfo = request.getPathInfo();
-		if (pathInfo != null
-				&& pathInfo.equals("/" + javascriptProvider.getCodeName())) {
-			// Serve icepush.js
-			serveIcePushCode(request, response);
-			return;
-		}
-
-		if (request.getRequestURI().endsWith(".icepush")) {
-			// Push request
-			try {
-				mainservlet.service(request, response);
-			} catch (ServletException e) {
-				throw e;
-			} catch (IOException e) {
-				throw e;
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		} else {
-			// Vaadin request
-			super.service(request, response);
-		}
-	}
-
-	private void serveIcePushCode(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
-
-		String icepushJavscript = javascriptProvider.getJavaScript();
-
-		response.setHeader("Content-Type", "text/javascript");
-		response.getOutputStream().write(icepushJavscript.getBytes());
-	}
-
-	@Override
-	public void destroy() {
-		super.destroy();
-		mainservlet.shutdown();
 	}
 }
