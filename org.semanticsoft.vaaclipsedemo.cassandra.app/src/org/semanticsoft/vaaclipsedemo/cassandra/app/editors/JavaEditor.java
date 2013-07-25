9763adba2e22d51548a2502741b67aa486f7f605
Rushan R. Gilmullin <semanticsoft.org@gmail.com>
2013-07-25 11:13:25 +0400

    add codelabel addon (vaadin 7 version) and replace the jhilight hilighting on codelabel for java files

--- org.semanticsoft.vaaclipsedemo.cassandra.app/src/org/semanticsoft/vaaclipsedemo/cassandra/app/editors/JavaEditor.java
+++ org.semanticsoft.vaaclipsedemo.cassandra.app/src/org/semanticsoft/vaaclipsedemo/cassandra/app/editors/JavaEditor.java
@@ -11,26 +11,28 @@
 
 package org.semanticsoft.vaaclipsedemo.cassandra.app.editors;
 
-import javax.inject.Inject;
-
+import com.porotype.codelabel.CodeLabel;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
+import javax.inject.Inject;
 import org.eclipse.e4.ui.model.application.ui.basic.MInputPart;
 
 /**
  * @author rushan
  *
  */
-public class JavaEditor extends JHighlightEditor
+public class JavaEditor extends TextEditor
 {
 	@Inject
 	public JavaEditor(VerticalLayout container, MInputPart inputPart)
 	{
-		super(container, inputPart);
-	}
-
-	@Override
-	String getJHighlighTypeName()
-	{
-		return "java";
+		super(inputPart.getInputURI());
+		Panel e = new Panel();
+		e.setSizeFull();
+		//text = new CodeLabel(readContent());
+		container.addComponent(e);
+		
+		CodeLabel text = new CodeLabel(readContent());
+		e.setContent(text);
 	}
 }
