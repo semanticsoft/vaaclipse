4386d2e9fd0c86734b8236f9639cdb4668928140
Klevis <klevis>
2013-08-15 17:03:09 +0200

    Add p2 Install Software functionality

---
+++ org.semanticsoft.vaaclipse.p2.service/src/org/semanticsoft/vaaclipse/p2/service/impl/InstallNewSoftwareService.java
@@ -0,0 +1,103 @@
+package org.semanticsoft.vaaclipse.p2.service.impl;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.equinox.internal.p2.metadata.repository.MetadataRepositoryManager;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.core.ProvisionException;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.operations.InstallOperation;
+import org.eclipse.equinox.p2.operations.ProvisioningSession;
+import org.eclipse.equinox.p2.query.IQuery;
+import org.eclipse.equinox.p2.query.IQueryResult;
+import org.eclipse.equinox.p2.query.QueryUtil;
+import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
+import org.semanticsoft.vaaclipse.p2.iservice.IInstallNewSoftwareService;
+
+public class InstallNewSoftwareService implements IInstallNewSoftwareService {
+	NullProgressMonitor nullProgressMonitor = new NullProgressMonitor();
+
+	URI uri;
+
+	IProvisioningAgent agent;
+
+	@Override
+	public List<IInstallableUnit> loadRepository(String uriString,
+			IProvisioningAgent agent) {
+
+		this.agent = agent;
+		try {
+			uri = new URI(uriString);
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		MetadataRepositoryManager metadataRepositoryManager = new MetadataRepositoryManager(
+				agent);
+		IMetadataRepository loadRepository = null;
+		try {
+			loadRepository = metadataRepositoryManager.loadRepository(uri, 0,
+					nullProgressMonitor);
+		} catch (ProvisionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		IQuery<IInstallableUnit> createQuery = QueryUtil.createIUGroupQuery();
+		IQueryResult<IInstallableUnit> query = loadRepository.query(
+				createQuery, nullProgressMonitor);
+		List<IInstallableUnit> list = new ArrayList<IInstallableUnit>();
+		for (IInstallableUnit iInstallableUnit : query) {
+
+			list.add(iInstallableUnit);
+
+		}
+
+		return list;
+
+	}
+
+	@Override
+	public String installNewSoftware(
+			List<IInstallableUnit> listIInstallableUnits) {
+
+		if (agent == null || listIInstallableUnits == null || uri == null) {
+
+			throw new IllegalArgumentException(
+					"You must first call load repository");
+		}
+		String resolutionDetails = "";
+		try {
+			final ProvisioningSession session = new ProvisioningSession(agent);
+			InstallOperation installOperation = new InstallOperation(session,
+					listIInstallableUnits);
+
+			installOperation.getProvisioningContext().setArtifactRepositories(
+					new URI[] { uri });
+			installOperation.getProvisioningContext().setMetadataRepositories(
+					new URI[] { uri });
+
+			IStatus resolveModal = installOperation
+					.resolveModal(nullProgressMonitor);
+
+			resolutionDetails = installOperation.getResolutionDetails();
+		} catch (IllegalArgumentException runtimeException) {
+			throw runtimeException;
+		}
+		return resolutionDetails;
+	}
+
+	@Override
+	public String loadAndInstallNewSoftware(String uriString,
+			IProvisioningAgent agent) {
+
+		return installNewSoftware(loadRepository(uriString, agent));
+	}
+
+}
---
+++ org.semanticsoft.vaaclipsedemo.mediaplayer/src/org/semanticsoft/vaaclipsedemo/mediaplayer/handlers/help/InstallNewSoftwareHandler.java
@@ -0,0 +1,141 @@
+package org.semanticsoft.vaaclipsedemo.mediaplayer.handlers.help;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.semanticsoft.vaaclipse.p2.iservice.IInstallNewSoftwareService;
+import org.semanticsoft.vaaclipsedemo.mediaplayer.MediaplayerActivator;
+import org.semanticsoft.vaadin.optiondialog.OptionDialog;
+
+import com.vaadin.event.FieldEvents;
+import com.vaadin.event.FieldEvents.BlurEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Tree;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class InstallNewSoftwareHandler {
+	@Inject
+	IProvisioningAgent provisioningAgent;
+
+	@Execute
+	public void execute(UI ui, final IInstallNewSoftwareService installService) {
+
+		Window window = new Window("Install new Software");
+
+		VerticalLayout mainLayout = new VerticalLayout();
+		window.setContent(mainLayout);
+		window.setModal(true);
+		window.setPositionX(500);
+		window.setPositionY(100);
+		mainLayout.setWidth("600px");
+		mainLayout.setHeight("450px");
+
+		Label label = new Label("Enter URI here");
+
+		final TextField textField = new TextField();
+
+		CssLayout cssLayout = new CssLayout();
+		cssLayout.addComponent(label);
+		cssLayout.addComponent(textField);
+		final Tree tree = new Tree("List of categories");
+		tree.setMultiSelect(true);
+
+		mainLayout.addComponent(cssLayout);
+		mainLayout.addComponent(tree);
+		tree.setImmediate(true);
+		textField.setImmediate(true);
+		Button button = new Button("Install");
+		mainLayout.addComponent(button);
+
+		final TextArea textArea = new TextArea("Error report");
+		mainLayout.addComponent(textArea);
+		textArea.setImmediate(true);
+		textArea.setSizeFull();
+
+		final List<IInstallableUnit> loadRepository = new ArrayList<>();
+		textField.addBlurListener(new FieldEvents.BlurListener() {
+
+			@Override
+			public void blur(BlurEvent event) {
+				// TODO Auto-generated method stub
+
+				String value = textField.getValue();
+				if (value != null && !value.trim().isEmpty()) {
+					loadRepository.addAll(installService.loadRepository(value,
+							provisioningAgent));
+
+					for (IInstallableUnit iInstallableUnit : loadRepository) {
+
+						tree.addItem(iInstallableUnit.getId() + " "
+								+ iInstallableUnit.getVersion());
+					}
+				}
+			}
+		});
+
+		button.addClickListener(new Button.ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+
+				List<IInstallableUnit> listFinal = new ArrayList<>();
+				Set values = (Set) tree.getValue();
+				for (Object string : values) {
+					System.out.println(string);
+					for (IInstallableUnit iInstallableUnit : loadRepository) {
+
+						if (string.equals(iInstallableUnit.getId() + " "
+								+ iInstallableUnit.getVersion())) {
+							System.out.println("Ok found it");
+							listFinal.add(iInstallableUnit);
+						}
+					}
+
+				}
+				textArea.setValue("");
+
+				String installNewSoftware = "";
+				try {
+					installNewSoftware = installService
+							.installNewSoftware(listFinal);
+
+				} catch (IllegalArgumentException exception) {
+
+					exception.printStackTrace();
+					if (exception.getMessage().contains(
+							"Profile id _SELF_ is not registered"))
+
+						textArea.setValue("You must export via .product file first");
+					else
+						textArea.setValue(exception.getMessage());
+
+					return;
+
+				}
+
+				textArea.setValue(installNewSoftware);
+
+			}
+		});
+
+		ui.addWindow(window);
+
+	}
+
+}
