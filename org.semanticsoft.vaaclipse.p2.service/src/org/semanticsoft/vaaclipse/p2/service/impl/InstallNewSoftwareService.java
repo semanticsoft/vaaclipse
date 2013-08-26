0017cd203fcbcc47e83f484bf4b29c5112dd5dd4
Klevis <klevis>
2013-08-26 16:48:01 +0200

    eclipse copyright

--- org.semanticsoft.vaaclipse.p2.service/src/org/semanticsoft/vaaclipse/p2/service/impl/InstallNewSoftwareService.java
+++ org.semanticsoft.vaaclipse.p2.service/src/org/semanticsoft/vaaclipse/p2/service/impl/InstallNewSoftwareService.java
@@ -29,6 +29,16 @@ import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
 import org.semanticsoft.vaaclipse.p2.iservice.IInstallNewSoftwareService;
 
 
+/*******************************************************************************
+ * Copyright (c) 2012 Klevis Ramo and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Klevis Ramo - initial API and implementation
+ *******************************************************************************/
 public class InstallNewSoftwareService implements IInstallNewSoftwareService {
 	NullProgressMonitor nullProgressMonitor;
 
--- org.semanticsoft.vaaclipse.p2.ui/src/org/semanticsoft/vaaclipse/p2/install/ui/impl/LicenseView.java
+++ org.semanticsoft.vaaclipse.p2.ui/src/org/semanticsoft/vaaclipse/p2/install/ui/impl/LicenseView.java
@@ -17,6 +17,16 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
 
+/*******************************************************************************
+ * Copyright (c) 2012 Klevis Ramo and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Klevis Ramo - initial API and implementation
+ *******************************************************************************/
 public class LicenseView implements ILicenseView {
 
 	HorizontalSplitPanel horizontalSplitPanel = new HorizontalSplitPanel();
