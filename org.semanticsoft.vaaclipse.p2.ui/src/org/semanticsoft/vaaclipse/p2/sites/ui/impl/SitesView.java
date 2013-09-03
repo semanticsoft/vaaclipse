716463684b08414f02f4680f792b872adfdd05be
Klevis <klevis>
2013-09-03 10:36:47 +0200

    add license

--- org.semanticsoft.vaaclipse.p2.ui/src/org/semanticsoft/vaaclipse/p2/sites/ui/impl/SitesView.java
+++ org.semanticsoft.vaaclipse.p2.ui/src/org/semanticsoft/vaaclipse/p2/sites/ui/impl/SitesView.java
@@ -13,7 +13,16 @@ import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TreeTable;
 import com.vaadin.ui.VerticalLayout;
-
+/*******************************************************************************
+ * Copyright (c) 2012 Klevis Ramo and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Klevis Ramo - initial API and implementation
+ *******************************************************************************/
 public class SitesView implements ISitesView {
 
 	VerticalLayout mainLayout = new VerticalLayout();
