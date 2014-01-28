e70ef20946e6b9dc87250bd86841364d9889c193
semanticsoft <semanticsoft.org@gmail.com>
2014-01-28 16:47:15 +0400

    exported file extension

--- org.lunifera.vaaclipse.ui.preferences.addon/src/org/lunifera/vaaclipse/ui/preferences/addon/internal/impexp/ExportPreferences.java
+++ org.lunifera.vaaclipse.ui.preferences.addon/src/org/lunifera/vaaclipse/ui/preferences/addon/internal/impexp/ExportPreferences.java
@@ -5,34 +5,28 @@ package org.lunifera.vaaclipse.ui.preferences.addon.internal.impexp;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.internal.preferences.PreferencesService;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IPreferenceFilter;
 import org.lunifera.vaaclipse.ui.preferences.addon.internal.PrefHelper;
 import org.lunifera.vaaclipse.ui.preferences.model.PreferencesPage;
-import org.osgi.framework.Bundle;
-import org.osgi.service.prefs.Preferences;
-import org.osgi.service.prefs.PreferencesService;
 import org.semanticsoft.vaadin.optiondialog.OptionDialog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.server.FileDownloader;
-import com.vaadin.server.Resource;
 import com.vaadin.server.StreamResource;
 import com.vaadin.server.StreamResource.StreamSource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  * @author rushan
@@ -67,12 +61,12 @@ public class ExportPreferences extends BasicImpExp {
             	if (selectedPages.isEmpty())
             		return null;
             	
-            	IEclipsePreferences root = org.eclipse.core.internal.preferences.PreferencesService.getDefault().getRootNode();
+            	IEclipsePreferences root = PreferencesService.getDefault().getRootNode();
             	
             	ByteArrayOutputStream baos = new ByteArrayOutputStream(200);
             	
             	try {
-					org.eclipse.core.internal.preferences.PreferencesService.getDefault().exportPreferences(root, new IPreferenceFilter[] {createFilter(selectedPages)}, baos);
+					PreferencesService.getDefault().exportPreferences(root, new IPreferenceFilter[] {createFilter(selectedPages)}, baos);
 				} catch (CoreException e) {
 					logger.error("Exception when export preferences", e);
 					return null;
@@ -84,7 +78,7 @@ public class ExportPreferences extends BasicImpExp {
             	return is;
                 
             }
-        }, "preferences");
+        }, "preferences.epf");
     }
 
 	protected IPreferenceFilter createFilter(List<PreferencesPage> selectedPages) {
@@ -102,7 +96,7 @@ public class ExportPreferences extends BasicImpExp {
 			}
 			
 			@Override
-			public Map getMapping(String scope) {return null;}
+			public Map<?, ?> getMapping(String scope) {return null;}
 		};
 	}
 
